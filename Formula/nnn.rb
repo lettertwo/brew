d4c2cafb561c437fa9c77bc27679daf7811a8b45
Eric Eldredge <lettertwo@gmail.com>
2022-06-22 14:59:25 -0400

    Add nnn formula
    Builds nnn with nerdfont support

---
+++ Formula/nnn.rb
@@ -0,0 +1,37 @@
+class Nnn < Formula
+  desc "Tiny, lightning fast, feature-packed file manager"
+  homepage "https://github.com/jarun/nnn"
+  url "https://github.com/jarun/nnn/archive/v4.5.tar.gz"
+  sha256 "fadc15bd6d4400c06e5ccc47845b42e60774f368570e475bd882767ee18749aa"
+  license "BSD-2-Clause"
+  head "https://github.com/jarun/nnn.git", branch: "master"
+
+  depends_on "gnu-sed"
+  depends_on "ncurses"
+  depends_on "readline"
+
+  def install
+    system "make", "install", "O_NERD=1", "PREFIX=#{prefix}"
+
+    bash_completion.install "misc/auto-completion/bash/nnn-completion.bash"
+    zsh_completion.install "misc/auto-completion/zsh/_nnn"
+    fish_completion.install "misc/auto-completion/fish/nnn.fish"
+
+    pkgshare.install "misc/quitcd"
+  end
+
+  test do
+    # Test fails on CI: Input/output error @ io_fread - /dev/pts/0
+    # Fixing it involves pty/ruby voodoo, which is not worth spending time on
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    # Testing this curses app requires a pty
+    require "pty"
+
+    (testpath/"testdir").mkdir
+    PTY.spawn(bin/"nnn", testpath/"testdir") do |r, w, _pid|
+      w.write "q"
+      assert_match "~/testdir", r.read
+    end
+  end
+end
