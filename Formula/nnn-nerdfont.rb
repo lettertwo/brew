ac4adb375b8252935d66e09c58fdfa029d0145d3
Eric Eldredge <lettertwo@gmail.com>
2022-06-22 15:11:56 -0400

    Update nnn-nerdfont.rb

--- Formula/nnn-nerdfont.rb
+++ Formula/nnn-nerdfont.rb
@@ -1,8 +1,6 @@
 class Nnn < Formula
   desc "Tiny, lightning fast, feature-packed file manager"
   homepage "https://github.com/jarun/nnn"
-  url "https://github.com/jarun/nnn/archive/v4.5.tar.gz"
-  sha256 "fadc15bd6d4400c06e5ccc47845b42e60774f368570e475bd882767ee18749aa"
   license "BSD-2-Clause"
   head "https://github.com/jarun/nnn.git", branch: "master"
 
@@ -18,20 +16,5 @@ class Nnn < Formula
     fish_completion.install "misc/auto-completion/fish/nnn.fish"
 
     pkgshare.install "misc/quitcd"
-  end
-
-  test do
-    # Test fails on CI: Input/output error @ io_fread - /dev/pts/0
-    # Fixing it involves pty/ruby voodoo, which is not worth spending time on
-    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
-
-    # Testing this curses app requires a pty
-    require "pty"
-
-    (testpath/"testdir").mkdir
-    PTY.spawn(bin/"nnn", testpath/"testdir") do |r, w, _pid|
-      w.write "q"
-      assert_match "~/testdir", r.read
-    end
   end
 end
